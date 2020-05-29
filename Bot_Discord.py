f43a7b91d41c2a984c5fac3a133e623b17a9b195
_[N_F]_ <48897493+xtvlw@users.noreply.github.com>
2020-05-29 14:04:40 -0300

    Update Bot_Discord.py

--- Bot_Discord.py
+++ Bot_Discord.py
@@ -1,25 +1,43 @@
-import discord
 from random import randrange
+from time import sleep
+import discord
 import sqlite3
 
+
 element, NoneNumbers = "", []
+
 args = ["lolis_images", "oppais_images", "hentai_images",
         "lolis_gifs", "oppais_gifs", "hentai_gifs"]
+
+Commands = [".loli", ".oppai", ".hentai",
+            ".gif-loli", ".gif-oppai", ".gif-hentai"]
+Max_lens = [100, 100, 100, 100, 37, 100]
 
 data = sqlite3.connect("DataBase.db")
 cursor = data.cursor()
 
 client = discord.Client()
-token = "put your token here"
+token = "NzEwNDg0NTQzOTM2NjU5NTU4.XtBbiQ.zWAb4N-JTY6RFPVY3J75J4Aptjg"
+
+Help_Message = (".loli => send a loli photo\n"
+                ".oppai => send a oppai phote\n"
+                ".hentai => send a hentai photo\n"
+                ".gif-loli => send a loli gif\n"
+                ".gif-oppai => send a oppai gif\n"
+                ".gif-hentai => send a hentai gif\n")
 
 
 def function(max_len, table):
     global element, NoneNumbers
     element_position = randrange(1, max_len)
 
+    sleep(0.5)
     while element_position in NoneNumbers:
         element_position = randrange(1, max_len)
     NoneNumbers += [element_position]
+    if len(NoneNumbers) == 20:
+        NoneNumbers = []
+    sleep(0.5)
 
     element = cursor.execute(f"SELECT link FROM {table} WHERE id={element_position}")
     for i in element:
@@ -35,33 +53,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global element
     if message.author == client.user:
         return
 
-    if message.content.startswith('.loli'):
-        function(100, args[0])
-        await message.channel.send(element)
-    if message.content.startswith(".oppai"):
-        function(100, args[1])
-        await message.channel.send(element)
-    if message.content.startswith(".hentai"):
-        function(100, args[2])
-        await message.channel.send(f'||{element}||')
-    if message.content.startswith(".gif-loli"):
-        function(100, args[3])
-        await message.channel.send(element)
-    if message.content.startswith(".gif-oppai"):
-        function(37, args[4])
-        await message.channel.send(element)
-    if message.content.startswith(".gif-hentai"):
-        function(100, args[5])
-        await message.channel.send(f'||{element}||')
-    if message.content.startswith("-h") or message.content.startswith("--help"):
-        await message.channel.send(".loli => send a loli photo\n"
-                                   ".oppai => send a oppai phote\n"
-                                   ".hentai => send a hentai photo\n"
-                                   ".gif-loli => send a loli gif\n"
-                                   ".gif-oppai => send a oppai gif\n"
-                                   ".gif-hentai => send a hentai gif\n")
+    for i in range(len(Commands)):
+        if message.content.startswith("-h") or message.content.startswith("--help"):
+            await message.channel.send(Help_Message)
+            break
+        if message.content.startswith(Commands[i]):
+            function(Max_lens[i], args[i])
+            if i == 2 or i == 5:
+                element = f"||{element}||"
+            await message.channel.send(element)
+            break
+
 
 client.run(token)
+data.close()
